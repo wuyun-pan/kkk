80e54c1e97f8372a466ccf60dcfbc5407b9cdc5d
wuyun-pan <103370703+wuyun-pan@users.noreply.github.com>
2022-09-18 22:21:13 +0930

    Update o.py

--- o.py
+++ o.py
@@ -1,2 +1,10 @@
-msg = "Hello World"
-print(msg)
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+import os
+
+def hello_world(request):
+    name = os.environ.get('NAME')
+    if name == None or len(name) == 0
+      name = "world"
+    message = "hello, " + name + "!\n"
